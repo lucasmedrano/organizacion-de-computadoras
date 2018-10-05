1262faf6883b2e07546f967459a0f4ef1b02c941
Lucas Nicolas Medrano <lucasmedrano97@gmail.com>
2018-10-05 12:40:13 -0300

    Arreglar problemas en general

--- tp1/qsort.c
+++ tp1/qsort.c
@@ -13,24 +13,24 @@
 
 void mostrar_error_y_salir(char *mensaje_error, int numero_salida){
     fprintf(stderr, "%s\n", mensaje_error);
- //   exit(numero_salida);
+    exit(numero_salida);
 }
 
-void mostrar_version(){ printf("%s\n", VERSION);}
+void mostrar_version(){printf("%s\n", VERSION);}
 
-void mostrar_ayuda(){
-    printf("%s\n", MENSAJE_AYUDA);
-}
+void mostrar_ayuda(){printf("%s\n", MENSAJE_AYUDA);}
 
 int obtener_longitud_maxima(FILE* archivo){
+    //Obtiene longitud contando el \n o \0 del final
+    // ejemplo: para "hola", maximo = 5
     int maximo = 0;
     int actual = 0;
     int caracter;
-    while (caracter = fgetc(archivo) != EOF){
+    while ((caracter = fgetc(archivo)) != EOF){
         actual++;
         if (caracter == '\n'){
             if (actual > maximo) maximo = actual;
-            actual = 0
+            actual = 0;
         }
     }
     return maximo;
@@ -39,7 +39,7 @@ int obtener_longitud_maxima(FILE* archivo){
 int obtener_cantidad_palabras(FILE *archivo, int len){
     int cantidad = 0;
     char palabra[len];
-    while(fgets(palabra, len, archivo)) cantidad ++;
+    while(fscanf(archivo,"%s",palabra) != EOF) cantidad ++;
     return cantidad;
 }
 
@@ -47,22 +47,22 @@ void obtener_palabras(FILE* archivo_a_ordenar, char **lista_palabras, int len){
     int indice = 0;
     char palabra[len];
 
-    while(fgets(palabra, len, archivo_a_ordenar) != NULL){
-        *(palabra + strlen(palabra) - 1) = '\0';
-        lista_palabras[indice] = malloc(strlen(palabra));
+    while(fscanf(archivo_a_ordenar,"%s",palabra) != EOF){
+        *(palabra + strlen(palabra)) = '\0';
+        lista_palabras[indice] = malloc(strlen(palabra) + 1);
         strcpy(lista_palabras[indice], palabra);
         indice ++;
     }
 }
 
 void orgaqsort(char** izq, char** der){
-    if (izq < der) {
+    if (izq <= der) {
         char *aux;
         char **inicio = izq;
         char **fin = der;
         while (inicio < fin){
-            while (strcmp(*inicio, *izq) <= 0)inicio++; //falta hacer que compare enteros, que seria
-            while (strcmp(*fin, *izq) > 0) fin--;       //atoi(*inicio) <=/> atoi(*izq), y tambien hacer qsort general
+            while ((strcmp(*inicio, *izq) <= 0) && (inicio < der))inicio++; //falta hacer que compare enteros, que seria
+            while ((strcmp(*fin, *izq) > 0) && (fin > izq)) fin--;       //atoi(*inicio) <=/> atoi(*izq), y tambien hacer qsort general
             if (inicio < fin){
                 aux = *inicio;
                 *inicio = *fin;
@@ -88,25 +88,25 @@ int main(int argc, char *argv[]){
     if ((argc < 2) || argc > 5) {
         mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
     }
-    if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)){
+    else if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)){
         if(argc != 2) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         mostrar_ayuda();
         return 0;
     }
-    if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)){
+    else if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)){
         if(argc != 2) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         mostrar_version();
         return 0;
     }
 
-    if ((strcmp(argv[1], "-o") == 0) || (strcmp(argv[1], "--output") == 0)){
+    else if ((strcmp(argv[1], "-o") == 0) || (strcmp(argv[1], "--output") == 0)){
         if (argc != 4) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         nro_archivo_entrada = 3;
         nro_archivo_salida = 2;
         criterio_ordenamiento = 0;
     }
 
-    if (((strcmp(argv[2], "-o") == 0) || (strcmp(argv[2], "--output") == 0)) && ((strcmp(argv[1], "-n") == 0) || (strcmp(argv[1], "--numeric") == 0))) {
+    else if (((strcmp(argv[2], "-o") == 0) || (strcmp(argv[2], "--output") == 0)) && ((strcmp(argv[1], "-n") == 0) || (strcmp(argv[1], "--numeric") == 0))) {
         if (argc != 5) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         nro_archivo_entrada = 4;
         nro_archivo_salida = 3;
@@ -121,6 +121,7 @@ int main(int argc, char *argv[]){
         output = fopen(argv[nro_archivo_salida], "w");
         if (!output) mostrar_error_y_salir(mensaje_error_archivo_inexistente, salida_error_archivo_inexistente);
     }
+
     longitud = obtener_longitud_maxima(archivo);
     rewind(archivo);
     cantidad_palabras = obtener_cantidad_palabras(archivo, longitud);
@@ -129,10 +130,10 @@ int main(int argc, char *argv[]){
     obtener_palabras(archivo, lista_palabras, longitud);
     orgaqsort(lista_palabras, lista_palabras + cantidad_palabras -1);
     for (int i = 0; i < cantidad_palabras; i++){
-        fprintf(output, lista_palabras[i]);
-        fprintf(output, "\n");
+        fprintf(output, "%s\n",lista_palabras[i]);
         free(lista_palabras[i]);
     }
+    printf("%i\n", criterio_ordenamiento);
     fclose(archivo);
     fclose(output);
     free(lista_palabras);
