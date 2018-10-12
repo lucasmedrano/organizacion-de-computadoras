53ef9c5c0aa6e9c6f8770b0eeb3291a4686093b4
Lucas Nicolas Medrano <lucasmedrano97@gmail.com>
2018-10-12 15:39:06 -0300

    qsort en assembly anda para strings. Falta para numeros.

--- tp1/orgaqsortassembly.s
+++ tp1/orgaqsortassembly.s
@@ -1,134 +1,136 @@
-        .text
-        .align 2
-        .globl orgaqsortassembly
-        .ent orgaqsortassembly
-        
+    .text
+    .align 2
+    .globl orgaqsortassembly
+    .ent orgaqsortassembly
+
 orgaqsortassembly:  .set noreorder
-        .cpload $25
-        .set reorder      
-		subu    $sp, $sp, 32
-        sw      $ra, 24($sp)
-        sw      $fp, 20($sp)
-        sw      $gp, 16($sp)
+                    .cpload $25
+                    .set reorder
 
-        sw      $4, 32($sp)
-        sw      $5, 36($sp)
-        sw      $6, 40($sp)
+        subu    $sp, $sp, 48
+        sw      $ra, 40($sp)
+        sw      $fp, 36($sp)
+        sw      $gp, 32($sp)
 
-        add     $16, $0, $4        # s0 <- izq
-        add     $17, $0, $5        # s1 <- der
-        add     $20, $0, $6        # s4 <- num
-        subu    $9, $17, $16       # if (der <= izq){
-        blez    $9, end            # end}
+        sw      $4, 48($sp)
+        sw      $5, 52($sp)
+        sw      $6, 56($sp)
 
-        add     $18, $0, $4        # s2 <- inicio
-        add     $19, $0, $5        # s3 <- fin
+        subu    $8, $4, $5      # if (izq > der) {
+        bgtz    $8, end       # end }
 
+        add     $16, $0, $4     # $16 <- izq
+        add     $17, $0, $5     # $17 <- der
+        add     $18, $0, $4     # $18 <- inicio
+        add     $19, $0, $5     # $19 <- fin
+        add    $20, $0, $6      # $20 <- num
 
-while1: subu    $10, $18, $19       # if (inicio >= fin) {
-        bgez    $10, swap2          #  swap2 }
+while1: subu    $8, $18, $19        # if(inicio >= fin){
+        bgez    $8, swap2           # swap2 }
 
-while2: subu    $10, $17, $18       # if (inicio >= der){
-        blez    $10, while3         #  while3}
+while2: subu    $8, $18, $17
+        bgez    $8, while3
 
-        add     $4, $0, $18         # Cargo inicio e izq
-        add     $5, $0, $16         # y num para
-        add     $6, $0, $20         # comparar
-        jal     comparar            # Compara *inicio y *izq segun num y devuelve en $v0
+        add     $4, $0, $18
+        add     $5, $0, $16
+        jal     comparar
         blez    $2, aumentar_inicio
 
-while3: subu    $10, $19, $16       # if(fin <= izq){
-        blez    $10, if       	    #   swap1}
-
-        add     $4, $0, $19         # Cargo der para comparar
-        add		$5, $0, $16
-        add		$6, $0, $20
+while3: subu    $8, $19, $16
+        blez    $8, swap1
+        add     $4, $0, $19
+        add     $5, $0, $16
         jal     comparar
         bgtz    $2, decrementar_fin
-        
-if:		subu	$10, $18, $19
-		bltz	$10, swap1
-		b		while1
 
-swap1:  la		$21, 0($18)
-		la		$22, 0($19)
-		lw      $11, 0($21)         #Swap inicio-fin
+swap1:  subu    $8, $18, $19
+        bgez    $8, while1
+
+        la		$21, 0($18)
+        la     	$22, 0($19)
+        lw      $11, 0($21)         #Swap inicio-fin
         lw      $12, 0($22)
         sw      $11, 0($22)
         sw      $12, 0($21)
         b       while1
 
 swap2:  la		$21, 0($16)
-		la		$22, 0($19)
-		lw      $11, 0($21)         #Swap izq-fin
+        la		$22, 0($19)
+        lw      $11, 0($21)         #Swap izq-fin
         lw      $12, 0($22)
         sw      $11, 0($22)
         sw      $12, 0($21)
 
+llamada_rec_1:  sw      $16, 16($sp)     # izq al ABA
+                sw      $17, 20($sp)     # der al ABA
+                sw      $18, 24($sp)     # inicio al ABA
+                sw      $19, 28($sp)     # fin al ABA
 
-        addi    $19, $19, -4             # qsort(izq, fin-1, num)
-        add     $4, $0, $16
-        add     $5, $0, $19
-        add     $6, $0, $20
-        jal     orgaqsortassembly
+                addi    $19, $19, -4
+                add     $4, $0, $16
+                add     $5, $0, $19
+                jal     orgaqsortassembly
 
-        addi    $19, $19, 8              # qsort(fin+1, der, num)
-        add     $4, $0, $19
-        add     $5, $16, $17
-        add     $6, $0, $20
-        jal     orgaqsortassembly
+llamada_rec_2:  lw      $19, 28($sp)
+                lw      $17, 20($sp)
 
-end:	lw		$ra, 24($sp)
-        lw		$fp, 20($sp)
-        lw		$gp, 16($sp)
-        addiu	$sp, $sp, 32
-        j		$ra
+                addi    $19, $19, 4
+                add     $4, $0, $19
+                add     $5, $0, $17
+                jal     orgaqsortassembly
 
-decrementar_fin:    addi   $19, $19, -4
-                    b      while3
+end:            lw      $ra, 40($sp)
+                lw      $fp, 36($sp)
+                lw      $gp, 32($sp)
+                addi    $sp, $sp, 48
+                jr     $ra
 
 aumentar_inicio:    addi    $18, $18, 4
-                    b      while2
-         .end    orgaqsortassembly
+                    b       while2
 
-			.text
-			.align 2
-			.globl comparar
-			.ent comparar
+decrementar_fin:    addi    $19, $19, -4
+                    b       while3
+
+                .end    orgaqsortassembly
+
+                .text
+                .align 2
+                .globl comparar
+                .ent comparar
 comparar:   .set noreorder
-			.cpload $25
-			.set reorder
-			subu    $sp, $sp, 8
-            sw      $fp, 4($sp)
-            sw      $gp, 0($sp)
+            .cpload $25
+            .set reorder
+        subu    $sp, $sp, 8
+        sw      $fp, 4($sp)
+        sw      $gp, 0($sp)
 
-            sw      $4, 8($sp)
-            sw      $5, 12($sp)
-            sw      $6, 16($sp)
+        sw      $4, 8($sp)
+        sw      $5, 12($sp)
+        sw      $6, 16($sp)
 
-            la		$14, 0($4)
-            la		$15, 0($5)
-            lw      $9, 0($14)
-            lw      $10, 0($15)
-loop:       lbu     $11, 0($9)
-            lbu     $12, 0($10)
+        la		$14, 0($4)
+        la		$15, 0($5)
+        lw      $9, 0($14)
+        lw      $10, 0($15)
+loop:   lbu     $11, 0($9)
+        lbu     $12, 0($10)
 
-            subu    $13, $11, $12
-            bltz    $13, dev_menor
-            bgtz    $13, dev_mayor
-            beq     $11, $0, dev_cero       #habria que hacer una funcion comparar_numeros, porque esto con numeros no anda
+        subu    $13, $11, $12
+        bltz    $13, dev_menor
+        bgtz    $13, dev_mayor
+        beq     $11, $0, dev_cero       #habria que hacer una funcion comparar_numeros, porque esto con numeros no anda
 
-            addi    $9, $9, 1
-            addi    $10, $10, 1
+        addi    $9, $9, 1
+        addi    $10, $10, 1
 
-            add     $11, $0, $0
-            add     $11, $0, $0
-            b       loop
+        add     $11, $0, $0
+        add     $11, $0, $0
+        b       loop
 
-fin:        lw      $fp, 4($sp)
-            lw      $gp, 0($sp)
-            addi    $sp, $sp, 8
-            j       $ra
+fin:    lw      $fp, 4($sp)
+        lw      $gp, 0($sp)
+        addi    $sp, $sp, 8
+        jr       $ra
 
 dev_cero:   add     $2, $0, $0
             b fin
@@ -138,5 +140,5 @@ dev_menor:  addi     $2, $0, -1
 
 dev_mayor:  addi    $2, $0, 1
             b fin
-            
-            .end comparar
+
+    .end comparar
