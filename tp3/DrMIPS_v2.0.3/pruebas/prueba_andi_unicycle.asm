118da3970217f66d9bdc3f374d988daad47b2754
Lucas Nicolas Medrano <lucasmedrano97@gmail.com>
2018-12-05 01:06:35 -0300

    Inicio de informe

---
+++ tp3/DrMIPS_v2.0.3/pruebas/prueba_andi_unicycle.asm
@@ -0,0 +1,4 @@
+prueba_andi:	addi		$s0, $zero, 10
+			andi		$s3, $s0, 75
+			andi		$s4, $s0, 0
+			andi		$s5, $s0, 0xffffffff
---
+++ tp3/DrMIPS_v2.0.3/pruebas/prueba_jregs_unicycle.asm
@@ -0,0 +1,3 @@
+prueba_jregs1:	addi		$s0, $zero, 70
+			addi		$s1, $zero, 80
+			jregs	$s0, $s1
